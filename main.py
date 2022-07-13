e4e3e62be31aea474942bf46d31619c97e964248
Viktor <vikt.mag@gmail.com>
2022-07-14 00:55:26 +0200

    Added visual representation of a group and functions to add and remove entries from group

--- main.py
+++ main.py
@@ -12,20 +12,24 @@ with open ("saved_groups.json", "r", encoding="utf-8") as file:
 def add_file():
     filepath = filedialog.askopenfilename(initialdir=DESKTOP, title="Select File", 
                                          filetypes=[("All Files", "*.*")])
-    get_filename = filepath.split("/",-2)
-    filename = get_filename[-2]+"/"+get_filename[-1]
     if filepath != "":
-        groups["new"][filepath] = filename
+        get_filename = filepath.split("/",-2)
+        filename = get_filename[-2]+"/"+get_filename[-1]
+        groups["new"][filename] = filepath
         write_json(groups)
 
 def add_folder():
     folderpath = filedialog.askdirectory(initialdir=DESKTOP, title="Select Folder")
-    get_foldername = folderpath.split("/")
-    foldername = get_foldername[-1]
     if folderpath != "":
-        groups["new"][folderpath] = foldername
+        get_foldername = folderpath.split("/")
+        foldername = get_foldername[-1]
+        groups["new"][foldername] = folderpath
         write_json(groups)
 
+def remove_entry(entry):
+    if entry != None:
+        del groups["new"][entry]
+        write_json(groups)
 
 def add_group(new_group):
     groups[new_group] = {}
@@ -34,6 +38,7 @@ def add_group(new_group):
 def remove_group(group):
     del groups[group]
     write_json(groups)
+
 
 
 def write_json(write_to_json):
@@ -55,11 +60,14 @@ def draw_gui():
     root.geometry('%dx%d+%d+%d' % (w, h, x, y))
     root.resizable(width=False, height=False)
 
-    add_file_button = tk.Button(text="Add File", command=add_file)
+    add_file_button = tk.Button(text="Add File", command=lambda:[add_file(),update_file_list()])
     add_file_button.place(x=100, y=100)
 
-    add_folder_button = tk.Button(text="Add Folder", command=add_folder)
+    add_folder_button = tk.Button(text="Add Folder", command=lambda:[add_folder(), update_file_list()])
     add_folder_button.place(x=100, y=130)
+
+    remove_entry_button = tk.Button(text="Remove Entry", command=lambda:[remove_entry(get_file_selection()),update_file_list()])
+    remove_entry_button.place(x=100, y=160)
 
     add_group_entry = tk.Entry(width=30)
     add_group_entry.place(x=250, y=70)
@@ -70,8 +78,16 @@ def draw_gui():
     remove_group_button.place(x=250, y=130)
 
 
+    file_list = StringVar(value=[k for k, v in groups["new"].items()])
+    file_listbox = tk.Listbox(listvariable=file_list, width=40, selectmode="SINGLE")
+    file_listbox.place(x=450, y=100)
 
+    def get_file_selection():
+        if file_listbox.curselection() != ():
+            return file_listbox.get(file_listbox.curselection())
 
+    def update_file_list():
+        file_list.set([k for k, v in groups["new"].items()])
 
     root.mainloop()
 
